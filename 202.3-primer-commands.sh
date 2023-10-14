0d51fc624e857da406fd3308a925eba80b4754e5
maccampbell <campbellmatthewa@gmail.com>
2023-10-13 16:39:55 -0800

    .edited

--- 202.3-primer-commands.sh
+++ 202.3-primer-commands.sh
@@ -1,4 +1,4 @@
-perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/1BF26FDD87_S1132_L002_R1_001.fastq > outputs/202/1BF26FDD87_S1132edited
-perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/1C2D44F136_S974_L002_R1_001.fastq > outputs/202/1C2D44F136_S974edited
-perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/7F7D3F627C_S1313_L002_R1_001.fastq > outputs/202/7F7D3F627C_S1313edited
-perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/127975561A_S1362_L002_R1_001.fastq > outputs/202/127975561A_S1362edited
+perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/1BF26FDD87_S1132_L002_R1_001.fastq > outputs/202/1BF26FDD87_S1132.edited
+perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/1C2D44F136_S974_L002_R1_001.fastq > outputs/202/1C2D44F136_S974.edited
+perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/7F7D3F627C_S1313_L002_R1_001.fastq > outputs/202/7F7D3F627C_S1313.edited
+perl ./data-processing-files/GTseq_Genotyper_v3_Tetra.pl 202.1-primer-loci.csv outputs/202/127975561A_S1362_L002_R1_001.fastq > outputs/202/127975561A_S1362.edited
